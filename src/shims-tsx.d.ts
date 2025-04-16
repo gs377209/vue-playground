91d679cb24589b1b9ee13f6942d5843f64bd5b71
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2025-04-16 14:26:28 -0400

    more upgrades

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -3,8 +3,10 @@ import Vue, { VNode } from "vue";
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
+    // eslint-disable-next-line
     interface Element extends VNode {}
     // tslint:disable no-empty-interface
+    // eslint-disable-next-line
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
       [elem: string]: any;
